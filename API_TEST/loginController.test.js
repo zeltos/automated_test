9283309e07c6d36675ed41eab367e6dea0ca72f9
meddmed <medianto.jaelani@gmail.com>
2019-03-07 10:31:50 +0700

    first commit file

---
+++ API_TEST/loginController.test.js
@@ -0,0 +1,45 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const serverApi = 'https://wikwik-store.thefthing.com/app_mobile_stable/v2'
+chai.use(chaiHttp);
+
+describe('GET /app_mobile_stable/v2/getCart_b2c.php', () => { 
+  it('should response empty cart', (done)=> {
+    chai.request(serverApi)
+    .get('/getCart_b2c.php')
+    .end((err,res)=> {
+      should.not.exist(err);
+      res.should.have.status(201);
+      res.type.should.equal('application/json');
+      res.body.should.be.a('object');
+      res.should.be.json;
+      res.body.result.should.include.keys(
+        'quote_id', 'can_checkout'
+        );
+      done();
+    })
+  })
+})
+
+// for Login
+describe('GET /app_mobile_stable/v2/customer/login.php', () => { 
+  it('should response empty cart', (done)=> {
+    chai.request(serverApi)
+    .post('/customer/login.php')
+    .send({"body":{"email":"medianto.jaelani@gmail.com","password":"Sebelas20"}})
+    .end((err,res)=> {
+      should.not.exist(err);
+      res.should.have.status(200);
+      res.type.should.equal('application/json');
+      res.body.should.be.a('object');
+      res.should.be.json;
+      res.body.result.status_code.should.eql(200);
+      res.body.result.response.should.eql('success');
+      res.body.should.include.keys(
+        'token'
+        );
+      done();
+    })
+  })
+})
